79f1b993b67a10572778209d58a0afd1ecd8b6d2
Vishnu Putta <vishnu.reddy@carelon.com>
2024-05-20 11:19:15 -0500

    Updated code

--- backtobackswe/binary-tree-level-order-traversal.py
+++ backtobackswe/binary-tree-level-order-traversal.py
@@ -1,45 +1,63 @@
 # Given a binary tree as input return its level order traversal.
 
 
+# A node structure
+class TreeNode:
 
-def levelOrderTraversal(root):
-    '''
-    :type root: TreeNode
-    :rtype: list of list of int
-    '''
-    level = []
-    visited = []
+    # A utility function to create a new node
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-    if root == None:
-        return []
 
-    queue = []
-    queue.append(root)
+# Iterative Method to print the
+class Solution:
+    def levelOrderTraversal(self, root):
+        '''
+        :type root: TreeNode
+        :rtype: list of list of int
+        '''
 
-    while len(queue) != 0:
-        layer = []
-        layer_size = len(layer)
-        for i in range(0, layer_size):
-            current_node = queue.pop(0)
-            layer.append(current_node.val)
-            if current_node.left:
-                queue.append(current_node.left)
+        if root is None:
+            return []
 
-            if current_node.right:
-                queue.append(current_node.right)
+        levels_list = []
 
-        level.append(layer)
+        queue = []
+        queue.append(root)
 
-    return level
+        while len(queue) != 0:
+            current_layer = []
+
+            layer_size = len(queue)
+
+            for i in range(0, layer_size):
+                current_node = queue.pop(0)
+
+                current_layer.append(current_node.val)
+
+                if current_node.left:
+                    queue.append(current_node.left)
+
+                if current_node.right:
+                    queue.append(current_node.right)
+
+            levels_list.append(current_layer)
+        return levels_list
 
 
 if __name__ == "__main__" :
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
 
-    a = levelOrderTraversal(1)
+    sol = Solution()
 
-    # root.left = Node(2)
-    # root.right = Node(3)
-    # root.left.left = Node(4)
-    # root.left.right = Node(5)
-    print(a)
+    res = sol.levelOrderTraversal(root)
+
+    print(res)
+
 
